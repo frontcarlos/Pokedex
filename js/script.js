544787eda3a33819b951879d4c2b96b749152224
frontcarlos <frontcarlos@outlook.com>
2022-09-28 11:52:46 -0300

    else ainda não resolvido

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ let searchPokemon = 1;
 const fetchPokemon = async (pokemon) =>{
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-    if(APIResponse.status = 200){
+    if(APIResponse.status == 200){
         const data = await APIResponse.json();
         return data;
     }
@@ -25,14 +25,14 @@ const renderPokemon = async (pokemon) =>{
     const data = await fetchPokemon(pokemon);
 
     if (data) {
-        pokemonImage.style.display = 'block'
+        pokemonImage.style.display = 'block';
         pokemonName.innerHTML = data.name;
         pokemonNumber.innerHTML = data.id;
         pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
         input.value = '';
         searchPokemon = data.id;
     } else {
-        pokemonImage.style.display = 'none'
+        pokemonImage.style.display = 'none';
         pokemonName.innerHTML = 'Not Found :C';
         pokemonNumber.innerHTML = '';
     }
