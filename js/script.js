24e49a4e72135b5267825069a8a6ce5acbd2891e
frontcarlos <frontcarlos@outlook.com>
2022-09-28 10:53:34 -0300

    nome e id recuperado da api

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+
 const fetchPokemon = async (pokemon) =>{
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    console.log(APIResponse);
+    const data = await APIResponse.json();   
+    return data;
 }
 
-fetchPokemon();
+const renderPokemon = async (pokemon) =>{
+    
+    const data = await fetchPokemon(pokemon);
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+
+}
+
+renderPokemon('25')
